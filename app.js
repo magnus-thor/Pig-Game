4003099785442769d02e406d81b90c3b1401a400
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-28 09:45:19 +0100

    added next player function

--- app.js
+++ app.js
@@ -9,4 +9,57 @@ GAME RULES:
 
 */
 
-var c
+var scores, roundScore, activePlayer, diceDom;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+diceDom = document.querySelector('.dice');
+diceDom.style.display = 'none';
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+var current_player_roundScore = document.querySelector('#current-' + activePlayer);
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // var current_player_roundScore = document.querySelector('#current-' + activePlayer);
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+    if (dice !== 1) {
+        roundScore += dice;
+        current_player_roundScore.textContent = roundScore;
+    } else {
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    scores[activePlayer] += roundScore;
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-' + activeplayer).textContent = 'Winner!';
+        diceDom.style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+    });
+
+function nextPlayer() {
+    roundScore = 0;
+    this.current_player_roundScore.textContent = roundScore;
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    diceDom.style.display = 'none';
+}
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+//
+// var x = document.querySelector('#score-0').textContent;
